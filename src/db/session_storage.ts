4465e4278f0cf7768baf68d658abc26f24da0bc2
Carlos Quinones <cquinones100@gmail.com>
2025-07-21 16:13:04 -0400

    add session storage support

---
+++ src/db/session_storage.ts
@@ -0,0 +1,36 @@
+import { ConsumedDrink, DrinkName } from "../types";
+import { DbDate } from "../utils/db_date";
+import { Db, ParseCallback } from "./db";
+
+class SessionStorage implements Db {
+  async saveDrink(drink: ConsumedDrink, date: DbDate) {
+    const currentDrinks = this.getDrinks();
+    const record = `${date},${drink.drink.name},${drink.units}`;
+
+    currentDrinks.push(record);
+
+    window.sessionStorage.setItem('drinks', JSON.stringify(currentDrinks));
+
+    return Promise.resolve();
+  }
+
+  async parse(callback: ParseCallback) {
+    const currentDrinks = this.getDrinks();
+
+    callback(currentDrinks.map((line) => {
+      const [date, drinkName, units] = line.split(',');
+
+      return [
+        date as DbDate,
+        drinkName as DrinkName,
+        Number(units),
+      ];
+    }));
+  }
+  
+  private getDrinks(): string[] {
+    return JSON.parse(window.sessionStorage.getItem('drinks') || JSON.stringify([]));
+  }
+}
+
+export default SessionStorage;
